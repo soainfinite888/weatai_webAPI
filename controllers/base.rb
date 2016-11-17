e0dee7481c5e3706f98e45fdb57f183b2110a7fd
youyotsu <youyotsu@gmail.com>
2016-11-17 13:25:53 +0800

    base.rb

--- controllers/base.rb
+++ controllers/base.rb
@@ -9,21 +9,6 @@ class WeataiAPI < Sinatra::Base
 
   API_VER = 'api/v0.1'
 
-  configure do
-    #Econfig.env = settings.environment.to_s
-    #Econfig.root = settings.root
-    #CWB::INSTANT
-
-    #                         key:  config.AUTH_KEY)
-    
-    #改這裡改這裡
-    #weather = CWB::INSTANT.instant
-    #CWB::INSTANT.config.update(dataid: config.DATA_ID,
-    #                          key:  config.AUTH_KEY)
-
-
-  end
-
   get '/?' do
     "WeataiAPI latest version endpoints are at: /#{API_VER}/"
   end
