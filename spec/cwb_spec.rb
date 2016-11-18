80e5f46b952d662e9960cc796df2e7e1dbe9353d
youyotsu <youyotsu@gmail.com>
2016-11-18 19:51:40 +0800

    gemfile.lock update
    gemfile.lock update

--- spec/cwb_spec.rb
+++ spec/cwb_spec.rb
@@ -1,29 +1,7 @@
 require_relative 'spec_helper.rb'
-#require_relative '../app.rb'
 require 'yaml'
 
 describe 'CWB routes' do
-
-#......................VCR things........................
-=begin 
-  VCR.configure do |c|
-    c.cassette_library_dir = 'cassettes'
-    c.hook_into :webmock
-
-#    c.filter_sensitive_data('<KEY>') { CREDENTIALS[:key] }
-  end
-
-  before do
-    VCR.insert_cassette 'all_record', record: :new_episodes
-#    @cwb_api = CWB::CWBApi.config
-
-  end
-
-  after do
-    VCR.eject_cassette
-  end
-=end
-
   before do
     VCR.insert_cassette 'all_record', record: :new_episodes
   end
@@ -31,8 +9,6 @@ describe 'CWB routes' do
   after do
     VCR.eject_cassette
   end
-
-#......................VCR things........................
 
 SAD_DATA_ID = '00000'
 API_VER = 'api/v0.1'
@@ -41,28 +17,28 @@ API_VER = 'api/v0.1'
 
   it 'get latest version' do
     get '/'
-    #http 200 OK
     last_response.status.must_equal 200
-    #puts last_response.body
   end 
 
   it 'HAPPY: should get data' do
     get "#{API_VER}/C_weather"
-    #http 200 OK
-    #puts last_response
     last_response.status.must_equal 200
-    #data format should be json
     last_response.content_type.must_equal 'application/json'
-    
-    #_data[''].length.must_be :>, 0
-    #_data[''].length.must_be :>, 0
   end
   
   it 'SAD: should not found data' do
     get "#{API_VER}/C_weather/#{SAD_DATA_ID}"
-    #http 404 not find
     last_response.status.must_equal 404
-    
-    #last_response.body.must_include [fix]
+  end
+
+  it 'should load data into database' do
+    DB[:C_weather].delete
+    put '/#{API_VER}/C_weather/基隆',
+        {instant_weather: weather}.to_json,
+        'content_type' => 'application/json'
+    last_response.status.must_equal 200
+    C_weather.count.must_equal 1
+     
+
   end
 end
