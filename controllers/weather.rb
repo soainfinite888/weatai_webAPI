2676911a54d2c632e7c028f47a6f0e88903746e9
Neil <wer321011@gmail.com>
2016-11-20 10:24:39 -0500

    Finish refactoring part 2

--- controllers/weather.rb
+++ controllers/weather.rb
@@ -5,17 +5,8 @@ class WeataiAPI < Sinatra::Base
 
   #get all station weather data(from database)
   get "/#{API_VER}/weather/?" do
-=begin
-    begin
-      weathers = FindAllWeathers.call
-      content_type 'application/json'
-      AllWeatherRepresenter.new(weathers: weathers).to_json
-    rescue
-      content_type 'text/plain'
-      halt 404, "Instant weather not found"
-    end
-=end
     result = FindAllWeathers.call
+
     if result.success?
       AllWeatherRepresenter.new(weathers: result).to_json
     else
@@ -25,53 +16,12 @@ class WeataiAPI < Sinatra::Base
 
   #get only one station weather data(from database)
   get "/#{API_VER}/weather/:station/?" do
-    station = params[:station]
-    #station_num = (station.to_i.modulo(39)+1)
-    begin
-      weather = Weather.find(station: station)
-      #if there has no weather info. of this station in DB
-      halt 400, "weather of (station: #{station_num}) not found" unless weather
-      
-      #if there has weather info. of this station in DB, output it
-      content_type 'application/json'
-      { #id:         weather., 
-        station:    weather.station, 
-        stationName:weather.stationName,
-        city:       weather.city,                       #station's city
-        township:   weather.township,                   #station's township
-        temperature:weather.temperature,                #station's temperature
-        humidity:   weather.humidity,                   #relative humidity(HUMD)相對濕度
-        MIN_10:     weather.MIN_10,                     #10min rainfall十分鐘雨量
-        rainfall:   weather.rainfall,                   #station's rainfall(day)雨量
-        AirQuality: weather.AirQuality,                 #AirQuality 空氣品質(環保署)
-        Status:     weather.Status,                     #AirStatus
-        time:       weather.time }.to_json
-    rescue
-      halt 404, "Instant weather not found(by station)"
-    end
-  end
+    result = FindWeather.call(params[:station])
 
-  #get all station weather data(from internet)
-  get "/#{API_VER}/weather_i/?" do
-    begin
-      weather = CWB::INSTANT.instant
-      content_type 'application/json'
-      { instant_weather: weather}.to_json
-
-    rescue
-      content_type 'text/plain'
-      halt 404, "Instant weather not found"
-    end
-  end
-
-  #get only one station weather data(from internet)
-  get "/#{API_VER}/weather_i/:station/?" do
-    begin
-      weather = CWB::INSTANT.local(:station)
-      content_type 'application/json'
-      { instant_weather: weather}.to_json
-    rescue
-      halt 404, "Instant weather not found"
+    if result.success?
+      WeatherRepresenter.new(result.value).to_json
+    else
+      ErrorRepresenter.new(result.value).to_status_response
     end
   end
 
