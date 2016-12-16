51b0056c525c96ab44133ef7cc57f08744eb0033
Neil <wer321011@gmail.com>
2016-12-16 03:09:22 -0500

    Finished api modifying

--- controllers/user_weather.rb
+++ controllers/user_weather.rb
@@ -17,6 +17,12 @@ class WeataiAPI < Sinatra::Base
     end
   end
   get "/#{API_VER}/user_weather/all/?" do
-    all_user_weather = UserWeather.where(:upload_time => (Time.now-7200) .. Time.now).all
+    begin
+      all_user_weather = UserWeather.where(:upload_time => (Time.now-7200) .. Time.now).all
+      all_user_weather.to_json
+    rescue
+      content_type 'text/plain'
+      halt 400, "User weather could not be found"
+    end
   end
 end
