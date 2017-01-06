89ab531d941bd4bd4775af0dc3a20c9e0c0b6cee
Neil <wer321011@gmail.com>
2017-01-06 08:23:39 -0500

    finish worker

---
+++ services/create_user_weather.rb
@@ -0,0 +1,17 @@
+class CreateUserWeather
+  extend Dry::Monads::Either::Mixin
+
+  def self.call(params)
+    begin
+      weather = UserWeather.create(
+                  lat: params['lat'],
+        	  lng: params['lng'],
+                  icon: params['icon'],
+                  upload_time: params['upload_time'],
+                  ) 
+      Right(weather)
+    rescue     
+      Left(Error.new(:bad_request, 'User weather could not be saved'))
+    end
+  end
+end
